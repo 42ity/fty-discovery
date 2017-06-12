fb9bda4032de174e3299adaaa9fa8fa3fb1bff72
Tomas Halman <TomasHalman@eaton.com>
2017-06-12 21:03:23 +0200

    Problem: There is not main actor
    Solution: Implement ftydiscovery actor, main and protocol
    between them.
    
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- src/ftydiscovery.cc
+++ src/ftydiscovery.cc
@@ -1,21 +1,21 @@
 /*  =========================================================================
     ftydiscovery - Manages discovery requests, provides feedback
 
-    Copyright (C) 2014 - 2017 Eaton                                        
-                                                                           
-    This program is free software; you can redistribute it and/or modify   
-    it under the terms of the GNU General Public License as published by   
-    the Free Software Foundation; either version 2 of the License, or      
-    (at your option) any later version.                                    
-                                                                           
-    This program is distributed in the hope that it will be useful,        
-    but WITHOUT ANY WARRANTY; without even the implied warranty of         
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-    GNU General Public License for more details.                           
-                                                                           
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     =========================================================================
 */
 
@@ -31,25 +31,66 @@
 //  Structure of our class
 
 struct _ftydiscovery_t {
-    int filler;     //  Declare class properties here
+    mlm_client_t *mlm;
 };
 
+//  --------------------------------------------------------------------------
+//  ftydiscovery actor
+
+void
+ftydiscovery_actor (zsock_t *pipe, void *args)
+{
+    ftydiscovery_t *self = ftydiscovery_new();
+    zpoller_t *poller = zpoller_new (pipe, mlm_client_msgpipe (self->mlm), NULL);
+    zsock_signal (pipe, 0);
+
+    while (!zsys_interrupted) {
+        void *which = zpoller_wait (poller, -1);
+        if (which == pipe) {
+            zmsg_t *msg = zmsg_recv (pipe);
+            if (msg) {
+                char *cmd = zmsg_popstr (msg);
+                if (cmd) {
+                    if (streq (cmd, "$TERM")) {
+                        zstr_free (&cmd);
+                        zmsg_destroy (&msg);
+                        break;
+                    }
+                    zstr_free (&cmd);
+                }
+                else if (streq (cmd, "BIND")) {
+                    char *endpoint = zmsg_popstr (msg);
+                    char *myname = zmsg_popstr (msg);
+                    assert (endpoint && myname);
+                    mlm_client_connect (self->mlm, endpoint, 5000, myname);
+                    zstr_free (&endpoint);
+                    zstr_free (&myname);
+                }
+                zmsg_destroy (&msg);
+            }
+        }
+        else if (which == mlm_client_msgpipe (self->mlm)) {
+            zmsg_t *msg = mlm_client_recv (self->mlm);
+            zmsg_destroy (&msg);
+        }
+    }
+
+    ftydiscovery_destroy (&self);
+    zpoller_destroy (&poller);
+}
 
 //  --------------------------------------------------------------------------
 //  Create a new ftydiscovery
 
 ftydiscovery_t *
-ftydiscovery_new (void)
+ftydiscovery_new ()
 {
     ftydiscovery_t *self = (ftydiscovery_t *) zmalloc (sizeof (ftydiscovery_t));
     assert (self);
     //  Initialize class properties here
+    self->mlm = mlm_client_new ();
     return self;
 }
-
-
-//  --------------------------------------------------------------------------
-//  Destroy the ftydiscovery
 
 void
 ftydiscovery_destroy (ftydiscovery_t **self_p)
@@ -58,11 +99,13 @@ ftydiscovery_destroy (ftydiscovery_t **self_p)
     if (*self_p) {
         ftydiscovery_t *self = *self_p;
         //  Free class properties here
+        mlm_client_destroy (&self->mlm);
         //  Free object itself
         free (self);
         *self_p = NULL;
     }
 }
+
 
 //  --------------------------------------------------------------------------
 //  Self test of this class
@@ -74,25 +117,10 @@ ftydiscovery_test (bool verbose)
 
     //  @selftest
     //  Simple create/destroy test
-
-    // Note: If your selftest reads SCMed fixture data, please keep it in
-    // src/selftest-ro; if your test creates filesystem objects, please
-    // do so under src/selftest-rw. They are defined below along with a
-    // usecase (asert) to make compilers happy.
-    const char *SELFTEST_DIR_RO = "src/selftest-ro";
-    const char *SELFTEST_DIR_RW = "src/selftest-rw";
-    assert (SELFTEST_DIR_RO);
-    assert (SELFTEST_DIR_RW);
-    // Uncomment these to use C++ strings in C++ selftest code:
-    //std::string str_SELFTEST_DIR_RO = std::string(SELFTEST_DIR_RO);
-    //std::string str_SELFTEST_DIR_RW = std::string(SELFTEST_DIR_RW);
-    //assert ( (str_SELFTEST_DIR_RO != "") );
-    //assert ( (str_SELFTEST_DIR_RW != "") );
-    // NOTE that for "char*" context you need (str_SELFTEST_DIR_RO + "/myfilename").c_str()
-
-    ftydiscovery_t *self = ftydiscovery_new ();
+    zactor_t *self = zactor_new (ftydiscovery_actor, NULL);
     assert (self);
-    ftydiscovery_destroy (&self);
+    zclock_sleep (500);
+    zactor_destroy (&self);
     //  @end
     printf ("OK\n");
 }
