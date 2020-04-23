787fd3559cc5ff555ee36771bb36165a5f975606
Jim Klimov <jim@jimklimov.com>
2020-04-23 11:19:58 +0200

    Bump copyrights to 2020

--- include/fty_discovery_server.h
+++ include/fty_discovery_server.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     ftydiscovery - Manages discovery requests, provides feedback
 
-    Copyright (C) 2014 - 2017 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
